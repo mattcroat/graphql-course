4a69737118082414d67f4da12eb41e35e3a5455b
mattcroat <mattcroat.dev@gmail.com>
2019-06-29 17:44:45 +0200

    GraphQL API server with graphql-yoga

--- 01-graphql-basics/src/index.js
+++ 01-graphql-basics/src/index.js
@@ -1,1 +1,38 @@
-console.log('Hello, World!')
+import { GraphQLServer } from 'graphql-yoga'
+
+// type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`
+
+// resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query!'
+    },
+    name() {
+      return 'Matt Croat'
+    },
+    location() {
+      return 'Croatia'
+    },
+    bio() {
+      return 'Web developer from Croatia'
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+server.start(() => {
+  console.log('The server is up!')
+})
