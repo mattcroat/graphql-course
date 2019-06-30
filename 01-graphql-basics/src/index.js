4a333ce669777dbdb39a10694011ed84dfc38c00
mattcroat <mattcroat.dev@gmail.com>
2019-06-30 20:39:24 +0200

    Refactor objects to use spread operator

--- 01-graphql-basics/src/index.js
+++ 01-graphql-basics/src/index.js
@@ -168,9 +168,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age,
+        ...args,
       }
 
       users.push(user)
@@ -186,10 +184,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author,
+        ...args,
       }
 
       posts.push(post)
@@ -206,9 +201,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post,
+        ...args,
       }
 
       comments.push(comment)
