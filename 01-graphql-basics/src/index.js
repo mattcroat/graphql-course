8014d29504871e4a45a67bd76583fb4148b89926
mattcroat <mattcroat.dev@gmail.com>
2019-07-05 22:07:21 +0200

    Add delete post and comment mutations

--- 01-graphql-basics/src/index.js
+++ 01-graphql-basics/src/index.js
@@ -88,7 +88,9 @@ const typeDefs = `
     createUser(data: CreateUserInput!): User!
     deleteUser(id: ID!): User!
     createPost(data: CreatePostInput!): Post!
+    deletePost(id: ID!): Post!
     createComment(data: CreateCommentInput!): Comment!
+    deleteComment(id: ID!): Comment!
   }
 
   input CreateUserInput {
@@ -234,6 +236,19 @@ const resolvers = {
 
       return post
     },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex(post => post.id === args.id)
+
+      if (postIndex === -1) {
+        throw new Error('Post not found 💩')
+      }
+
+      const deletedPosts = posts.splice(postIndex, 1)
+
+      comments = comments.filter(comment => comment.post !== args.id)
+
+      return deletedPosts[0]
+    },
     createComment(parent, args, ctx, info) {
       const userExists = users.some(user => user.id === args.data.author)
       const postExists = posts.some(post => post.id === args.data.post && post.published)
@@ -250,6 +265,17 @@ const resolvers = {
       comments.push(comment)
 
       return comment
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(comment => comment.id === args.id)
+
+      if (commentIndex === -1) {
+        throw new Error('Comment not found 💩')
+      }
+
+      const deletedComments = comments.splice(commentIndex, 1)
+
+      return deletedComments[0]
     },
   },
   Post: {
